911e45d1adef57406ca17430704dd4ed1fe8bdad
Kyle <kylexiao20@gmail.com>
2021-12-09 16:48:06 -0500

    Use dotfile scripts for status bar

--- config.h
+++ config.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"",		"sb-memory",		30,			0},
 
-	{"", "date '+%a, %b %d %I:%M'",					5,		0},
+	{" ", 		"sb-time",		5,			0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
