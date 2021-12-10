1cb956154bc2c1641b541b5f819cb34f680d39ac
Kyle <kylexiao20@gmail.com>
2021-12-09 19:27:47 -0500

    Add battery status

--- config.h
+++ config.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",		"sb-memory",		30,			0},
-
-	{" ", 		"sb-time",		5,			0},
+	{"",		"sb-battery",		10,			0},
+	{"",		"sb-memory",		10,			0},
+	{"", 		"sb-time",		60,			0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " ";
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
